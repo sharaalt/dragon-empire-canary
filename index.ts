bc9c91a6d704a39251ede887b290ddb1b5bda061
sharaalt <yousif.sharafzada@student.tdsb.on.ca>
2023-03-26 12:21:11 -0400

    Updated index.js

--- index.ts
+++ index.ts
@@ -1,1 +1,11 @@
+import { Client, IntentsBitField } from "discord.js"
 
+const client = new Client({
+    intents: [
+        IntentsBitField.Flags.GuildMessages,
+        IntentsBitField.Flags.GuildMessageReactions,
+        IntentsBitField.Flags.Guilds,
+    ]
+})
+
+client.login(process.env.TOKEN)
